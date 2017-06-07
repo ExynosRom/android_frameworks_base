3a76669b80338c32106d8d64f4684c49a6d3b7c6
xyyx <xyyx@mail.ru>
2017-06-07 16:28:14 +0200

    Unlock keystore with fingerprint after reboot or SysUI restart ezio84: remove the settings part and set by default
    Change-Id: I4ee09d8103cad22edd960e201740839b071f2504

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3713,9 +3713,7 @@ public final class Settings {
           * @hide
           */
           public static final String RECENTS_USE_OMNISWITCH = "recents_use_omniswitch";
- 
-         /**
-
+		 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
